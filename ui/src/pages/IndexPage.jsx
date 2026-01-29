83f7a2fa81153f4461e1e6b332af6729bcc815ba
guts-718 <rahul.20223195@mnnit.ac.in>
2026-01-29 18:43:23 +0530

    rendered raw data on indexPage

--- ui/src/pages/IndexPage.jsx
+++ ui/src/pages/IndexPage.jsx
@@ -1,10 +1,19 @@
-//import { useItems } from "../DataProvider";
 import { useItems } from "../useItems";
 
 export default function IndexPage() {
   const { items, loading } = useItems();
-
   console.log("Items from DB:", items);
+  if (loading) return <h1>Loading...</h1>;
 
-  return <h1>Index Page hhehe</h1>;
+  return (
+    <div>
+      <h1>Index Page</h1>
+
+      {items.map(item => (
+        <div key={item.id}>
+          <b>{item.type}</b> | {item.file} | {item.text || item.content}
+        </div>
+      ))}
+    </div>
+  );
 }
